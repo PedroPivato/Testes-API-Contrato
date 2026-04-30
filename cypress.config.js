6a18e5390d098b92b918d0ed44df85bdaba6cc5f
Pedro Pivato <pivatopedro@gmail.com>
2026-04-30 19:23:12 -0300

    adicionado CI + allure report

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,13 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  allowCypressEnv: false,
+  allowCypressEnv: true,
 
   e2e: {
     baseUrl: "http://lojaebac.ebaconline.art.br/",
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require('@shelex/cypress-allure-plugin/writer')(on, config)
+      return config
     },
   },
 });
