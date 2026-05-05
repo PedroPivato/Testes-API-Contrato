ec1e0fddd75cf95762cf2e27bd84c858c08bfe5d
Pedro Pivato <pivatopedro@gmail.com>
2026-05-05 19:34:52 -0300

    correções do relatório

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,13 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   allowCypressEnv: true,
-
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: 'cypress/reports',
+    overwrite: false,
+    html: true,
+    json: false,
+  },
   e2e: {
     baseUrl: "http://lojaebac.ebaconline.art.br/",
     setupNodeEvents(on, config) {
